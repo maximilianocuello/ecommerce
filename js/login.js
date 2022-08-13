c47a36d4fb978cc7defddf57d94b56ceda161076
Maximiliano Cuello <110405289+maximilianocuello@users.noreply.github.com>
2022-08-13 14:30:34 -0300

    funcionalidad al login

--- js/login.js
+++ js/login.js
@@ -1,6 +1,15 @@
-const login = () => {
-    window.location.href = "index.html";
-}
-const SUBMIT= document.getElementById("submit");
 
-SUBMIT.addEventListener('click', login);
+const SUBMIT= document.getElementById('submit');
+const error = document.getElementById('incorrecto');
+
+
+SUBMIT.addEventListener('click', () => {
+    const inputEmail = document.getElementById('inputEmail').value;
+    const inputPassword = document.getElementById('inputPassword').value;
+    if(inputEmail && inputPassword){
+        window.location.href = "index.html";
+    }
+    else{
+         error.innerHTML = "Email o contraseña incorrectos";
+    }
+});
