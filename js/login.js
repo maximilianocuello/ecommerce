9065aaade610825766bbe1813450de35e825ece9
Maximiliano Cuello <cuellomaximiliano0@gmail.com>
2022-09-15 10:55:53 -0300

    change form

--- js/login.js
+++ js/login.js
@@ -18,17 +18,17 @@ const error = document.getElementById('incorrecto');
 
 
 
-function onSignIn(googleUser) {
-  // Useful data for your client-side scripts:
-  var profile = googleUser.getBasicProfile();
-  console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-  console.log('Full Name: ' + profile.getName());
-  console.log('Given Name: ' + profile.getGivenName());
-  console.log('Family Name: ' + profile.getFamilyName());
-  console.log("Image URL: " + profile.getImageUrl());
-  console.log("Email: " + profile.getEmail());
-
-  // The ID token you need to pass to your backend:
-  var id_token = googleUser.getAuthResponse().id_token;
-  console.log("ID Token: " + id_token);
+function handleCredentialResponse(response) {
+  console.log("Encoded JWT ID token: " + response.credential);
+}
+window.onload = function () {
+  google.accounts.id.initialize({
+    client_id: "564727709025-cjavt28rtr3niicv75cje2107dgdk4ga.apps.googleusercontent.com",
+    callback: handleCredentialResponse
+  });
+  google.accounts.id.renderButton(
+    document.getElementById("buttonDiv"),
+    { theme: "outline", size: "large" }  // customization attributes
+  );
+  google.accounts.id.prompt(); // also display the One Tap dialog
 }
